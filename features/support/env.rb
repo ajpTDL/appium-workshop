bcb8be3d12fa2e600388349de7abf44a7695fa83
Arturs Janis Petersons <atrurs.j.petersons@testdevlab.com>
2017-01-27 12:32:06 +0200

    merge conflicts

--- features/support/env.rb
+++ features/support/env.rb
@@ -18,7 +18,7 @@ end
 options = {
   'port' => 5500,
   'portboot' => 5051,
-  'sn' => '00bbb8a0ec4f9f25',
+  'sn' => '0915f9dd479e1d05',
   'app' => 'app.apk',
   'appPackage' => 'com.testdevlab.notifyus'
 
