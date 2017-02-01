072d23732029f54545076be5f3ac393c83fb577b
Arturs Janis Petersons <atrurs.j.petersons@testdevlab.com>
2017-02-01 11:25:49 +0200

    create function to navigate to main screen

--- features/screens/screen_create_filter.rb
+++ features/screens/screen_create_filter.rb
@@ -3,6 +3,10 @@ class ScreenCreateFilter < ScreenBase
   def initialize(driver)
     @title = element(:text, 'Izveidot filtru')
     @rows = element(:id, 'main_row_text')
+    @toolbar_actionbar = element(:id, 'toolbar_actionbar')
+    @toolbar_actionbar_categories = element(:id, 'toolbar_actionbar_categories')
+    @back_button = element(:class, 'android.widget.ImageButton')
+    @menu_item = element(:id, 'item_name')
     @driver = driver
   end
 
@@ -15,6 +19,28 @@ class ScreenCreateFilter < ScreenBase
       if row.text == category_name
         row.click
         break
+      end
+    end
+  end
+
+  def go_to_main
+    if @driver.find_elements(@toolbar_actionbar_categories[:type], @toolbar_actionbar_categories[:value]).size != 0
+      toolbar_category = @driver.find_element(@toolbar_actionbar_categories[:type], @toolbar_actionbar_categories[:value])
+      while toolbar_category do
+        toolbar_category.find_element(@back_button[:type], @back_button[:value]).click
+        if @driver.find_elements(@toolbar_actionbar_categories[:type], @toolbar_actionbar_categories[:value]).size != 0
+          toolbar_category = @driver.find_element(@toolbar_actionbar_categories[:type], @toolbar_actionbar_categories[:value])
+        else
+          toolbar_category = false
+        end
+      end
+    else
+      @driver.find_element(@toolbar_actionbar[:type], @toolbar_actionbar[:value]).find_element(@back_button[:type], @back_button[:value]).click
+      @driver.find_elements(@menu_item[:type], @menu_item[:value]).each do |menu_entry|
+        if menu_entry.text == @title[:value]
+          menu_entry.click
+          break
+        end
       end
     end
   end
